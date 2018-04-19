6b56e02c473a0d65df477bd1c9689b47b78c5d19
hcx07 <756170593@qq.com>
2018-04-19 14:06:53 +0800

    1

--- application/index/view/article/add.php
+++ application/index/view/article/add.php
@@ -1,10 +1,32 @@
-﻿<script type="text/javascript" charset="utf-8" src="/jquery/jquery.js"></script>
+﻿<!-- Bootstrap -->
+<link href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- NProgress -->
+<link href="/vendors/nprogress/nprogress.css" rel="stylesheet">
+<!-- iCheck -->
+<link href="/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+<!-- bootstrap-progressbar -->
+<link href="/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<!-- JQVMap -->
+<link href="/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+<!-- bootstrap-daterangepicker -->
+<link href="/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+<!-- Custom Theme Style -->
+<link href="/build/css/custom.min.css" rel="stylesheet">
+
+<script type="text/javascript" charset="utf-8" src="/jquery/jquery.js"></script>
+
+
 <script type="text/javascript" charset="utf-8" src="/ueditor/ueditor.config.js"></script>
 <script type="text/javascript" charset="utf-8" src="/ueditor/ueditor.all.js"> </script>
 <script type="text/javascript" charset="utf-8" src="/ueditor/lang/zh-cn/zh-cn.js"></script>
 
 <link href="/webuploader/dist/webuploader.css" rel="stylesheet">
 <script type="text/javascript" charset="utf-8" src="/webuploader/dist/webuploader.js"></script>
+
 <script type="text/javascript" charset="utf-8" src="/layer/src/layer.js"></script>
 <body class="nav-md" style="background: #ffffff">
 <div class="row" >
